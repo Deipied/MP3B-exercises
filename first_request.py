693f8a87654d7966843aca43c4b4f37b81bade5c
Hoon Cho <hoon.cho@ascentllc.local>
2021-08-02 14:59:01 -0500

    worked on first quest a bit more

--- first_request.py
+++ first_request.py
@@ -1,5 +1,13 @@
 import requests
-url = "http://www.google.com"
-response = requests.get(url)
+# url = "http://www.google.com"
+# response = requests.get(url)
 
-print(f"your request to {url} came back w/ status code {response.status_code}")
+# print(f"your request to {url} came back w/ status code {response.status_code}")
+
+url = "https://icanhazdadjoke.com"
+
+response = requests.get(url, headers = {"Accept":"application/json"})
+
+data = response.json()
+
+print(data["joke"])
